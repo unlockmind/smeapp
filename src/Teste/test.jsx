d8e1faae53c0205934c8c5487e8ee64117c9c825
AdilsonLopesSnir <antonio.lopes@snir.co.ao>
2020-03-17 13:06:08 +0100

    sasasa

--- src/Teste/test.jsx
+++ src/Teste/test.jsx
@@ -3,7 +3,31 @@ import FieldSME from "../components/@Fields";
 import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
 import { Form } from "@unform/web";
 import { useRef } from "react";
-
+const countryOptions = [
+  { value: "af", label: "Afghanistan" },
+  { value: "ax", label: "Aland Islands" },
+  { value: "al", label: "Albania" },
+  { value: "dz", label: "Algeria" },
+  { value: "as", label: "American Samoa" },
+  { value: "ad", label: "Andorra" },
+  { value: "ao", label: "Angola" },
+  { value: "ai", label: "Anguilla" },
+  { value: "ag", label: "Antigua" },
+  { value: "ar", label: "Argentina" },
+  { value: "am", label: "Armenia" },
+  { value: "aw", label: "Aruba" },
+  { value: "au", label: "Australia" },
+  { value: "at", label: "Austria" },
+  { value: "az", label: "Azerbaijan" },
+  { value: "bs", label: "Bahamas" },
+  { value: "bh", label: "Bahrain" },
+  { value: "bd", label: "Bangladesh" },
+  { value: "bb", label: "Barbados" },
+  { value: "by", label: "Belarus" },
+  { value: "be", label: "Belgium" },
+  { value: "bz", label: "Belize" },
+  { value: "bj", label: "Benin" }
+];
 export default function Test() {
   const testFomref = useRef(null);
   return (
@@ -19,9 +43,28 @@ export default function Test() {
               </p>
             </CardHeader>
             <CardBody className="all-icons">
-              <Form ref={testFomref}>
-                <FieldSME type="text" name="Audio" />
-                <FieldSME type="select" name="AudioSelect" options={[]} />
+              <Form
+                ref={testFomref}
+                onSubmit={e => {
+                  console.log(e);
+                }}
+              >
+                <FieldSME
+                  type="text"
+                  name="Audio"
+                  onChange={e => {
+                    console.log(e.target.value);
+                  }}
+                />
+                <FieldSME
+                  type="select"
+                  name="AudioSelect"
+                  options={countryOptions}
+                  onChange={e => {
+                    console.log(e.value);
+                  }}
+                />
+                <button type="submit">Entrar</button>
               </Form>
             </CardBody>
           </Card>
